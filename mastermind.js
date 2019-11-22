8d60e32e56fa6a7eb35b71023e20a98daa21c868
John Spruijt <99058314@mydavinci.nl>
2019-11-22 15:11:46 +0100

    fixed colourCode generator

--- mastermind.js
+++ mastermind.js
@@ -3,7 +3,7 @@ function br() {
         x.innerHTML = "<br>"
         page.appendChild(x)
 }
-var mainDiv = document.createElement("div")
+const mainDiv = document.createElement("div")
     document.body.appendChild(mainDiv)
     mainDiv.setAttribute('id',"page")
     var page = document.getElementById('page')
@@ -11,11 +11,11 @@ var mainDiv = document.createElement("div")
 
 
 for (i=0; i<12; i++) {
-    for (x=0; x<5; x++) {
+    for (y=0; y<4; y++) {
         var box = document.createElement("div")
             page.appendChild(box)
             box.setAttribute('class',"box")
-            box.setAttribute('id',"box"+i+x)
+            box.setAttribute('id',"box"+i+y)
     }
     br()
 }
@@ -62,3 +62,7 @@ br()
     x.setAttribute('class',"options")
     x.innerHTML = '<img src="images/White.png" alt="colour">'
     x.setAttribute('onclick',"alert('White')")
+
+
+var colours = ["red","green","blue","yellow","black","white"]
+var colourCode = [colours[Math.floor((Math.random() * 5) + 1)],colours[Math.floor((Math.random() * 5) + 1)],colours[Math.floor((Math.random() * 5) + 1)],colours[Math.floor((Math.random() * 5) + 1)]]
