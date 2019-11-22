615c388c4891cc842ee6f87dec49b9999a32ad6d
John Spruijt <99058314@mydavinci.nl>
2019-11-22 10:54:08 +0100

    added buttons and images

---
+++ mastermind.js
@@ -0,0 +1,51 @@
+function br() {
+    var x = document.createElement("a")
+        x.innerHTML = "<br>"
+        page.appendChild(x)
+}
+var mainDiv = document.createElement("div")
+    document.body.appendChild(mainDiv)
+    mainDiv.setAttribute('id',"page")
+    var page = document.getElementById('page')
+
+    x = document.createElement("button")
+    page.appendChild(x)
+    x.setAttribute('id',"colorRed")
+    x.setAttribute('class',"options")
+    x.innerHTML = '<img src="images/Red.png" alt="colour">'
+    x.setAttribute('onclick',"alert('Red')")
+
+    x = document.createElement("button")
+    page.appendChild(x)
+    x.setAttribute('id',"colorGreen")
+    x.setAttribute('class',"options")
+    x.innerHTML = '<img src="images/Green.png" alt="colour">'
+    x.setAttribute('onclick',"alert('Green')")
+
+    x = document.createElement("button")
+    page.appendChild(x)
+    x.setAttribute('id',"colorBlue")
+    x.setAttribute('class',"options")
+    x.innerHTML = '<img src="images/Blue.png" alt="colour">'
+    x.setAttribute('onclick',"alert('Blue')")
+
+    x = document.createElement("button")
+    page.appendChild(x)
+    x.setAttribute('id',"colorYellow")
+    x.setAttribute('class',"options")
+    x.innerHTML = '<img src="images/Yellow.png" alt="colour">'
+    x.setAttribute('onclick',"alert('Yellow')")
+
+    x = document.createElement("button")
+    page.appendChild(x)
+    x.setAttribute('id',"colorBlack")
+    x.setAttribute('class',"options")
+    x.innerHTML = '<img src="images/Black.png" alt="colour">'
+    x.setAttribute('onclick',"alert('Black')")
+
+    x = document.createElement("button")
+    page.appendChild(x)
+    x.setAttribute('id',"colorWhite")
+    x.setAttribute('class',"options")
+    x.innerHTML = '<img src="images/White.png" alt="colour">'
+    x.setAttribute('onclick',"alert('White')")
